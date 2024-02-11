da33c30b409eb69517a285bad0f90b23272517a8
Baron Afutu <afutubaronny@gmail.com>
2024-02-11 14:12:27 +0000

    Added main files

--- shell.h
+++ shell.h
@@ -12,18 +12,18 @@
 #include <fcntl.h>
 #include <errno.h>
 
-/* read/write buffers */
+/* for read/write buffers */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
-/* command chaining */
+/* for command chaining */
 #define CMD_NORM	0
 #define CMD_OR		1
 #define CMD_AND		2
 #define CMD_CHAIN	3
 
-/* convert_number() */
+/* for convert_number() */
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
 
